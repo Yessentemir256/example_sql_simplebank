619247a2e4501bf8d2108464ad313c58509393c8
Yessentemir <yessentemir.utepov@gmail.com>
2025-03-23 21:26:06 +0500

    upgraded

--- blog_database.sql
+++ blog_database.sql
@@ -5,22 +5,22 @@ CREATE TABLE "users" (
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
-  "created_at" timestamptz NOT NULL DEFAULT (now())
+  "created_at" timestampz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "essays" (
-  "id" bigserial PRIMARY KEY,
+  "essay_id" bigserial PRIMARY KEY,
   "title" varchar(255) NOT NULL,
   "content" text NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT (now())
+  "created_at" timestampz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "comments" (
-  "id" bigserial PRIMARY KEY,
+  "comment_id" bigserial PRIMARY KEY,
   "owner" bigint NOT NULL,
   "essay_id" bigint NOT NULL,
   "content" text NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT (now())
+  "created_at" timestampz NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "comments" ("essay_id");
@@ -29,4 +29,4 @@ CREATE INDEX ON "comments" ("owner");
 
 ALTER TABLE "comments" ADD FOREIGN KEY ("owner") REFERENCES "users" ("id");
 
-ALTER TABLE "comments" ADD FOREIGN KEY ("essay_id") REFERENCES "essays" ("id");
+ALTER TABLE "comments" ADD FOREIGN KEY ("essay_id") REFERENCES "essays" ("essay_id");
